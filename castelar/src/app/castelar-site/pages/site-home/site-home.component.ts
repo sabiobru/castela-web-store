7f6ba7bf3a5460bcfeb16fe9a6f4e489eb5f65dc
Bruno Sábio <45543767+sabiobru@users.noreply.github.com>
2021-04-21 02:25:39 -0300

    Ste Home

--- castelar/src/app/castelar-site/pages/site-home/site-home.component.ts
+++ castelar/src/app/castelar-site/pages/site-home/site-home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-site-home',
@@ -7,8 +6,32 @@ import { CarouselModule } from 'ngx-bootstrap/carousel';
   styleUrls: ['./site-home.component.scss']
 })
 export class SiteHomeComponent implements OnInit {
+  itemsPerSlide = 3;
+  slides = [
+    { name: 'slide 1', path: './../../../../assets/images/slide_1.jpg' },
+    { name: 'slide 2', path: './../../../../assets/images/slide_2.jpg' },
+    { name: 'slide 3', path: './../../../../assets/images/slide_3.jpg' }
+  ];
+
+
+  slidesProducts = [
+    { name: 'Produto 1', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 2', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 2', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 3', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 4', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 5', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 6', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 7', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 8', path: './../../../../assets/images/product_1.png' },
+    { name: 'Produto 9', path: './../../../../assets/images/product_1.png' },
+  ];
 
   constructor() { }
+
+
+
+
 
   ngOnInit(): void {
   }
